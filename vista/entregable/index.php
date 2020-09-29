0072d6d480ca2886cdb7b8ca36bf97e1bb893dec
GiselOtero <gisel.otero@est.fi.uncoma.edu.ar>
2020-09-29 03:34:46 -0300

    modificar index

---
+++ vista/entregable/index.php
@@ -0,0 +1,12 @@
+<?php
+    include_once ("../estructura/cabecera.php");
+?>
+<div style="width:90%;margin:10px auto; text-align: center">
+    <h1 class="font-weight-bold">FiDrive</h1>
+    <h2>Programaci&oacute;n Web Din&aacute;mica </h2>
+    <h4>Tecnicatura Universitaria en Desarrollo Web</h4>
+    <p class="text-muted">Esta es una aplicacion para evaluar mi progreso en la materia PWD 2020. Mi nombre es Gisel Otero y mi legajo es FAI-1753</p>
+</div>
+<?php
+    include_once ("../estructura/pie.php");
+?>
