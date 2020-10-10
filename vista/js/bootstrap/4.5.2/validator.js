6701d905a25c134572385ef36289cafd4d87e026
GiselOtero <gisel.otero@est.fi.uncoma.edu.ar>
2020-10-10 03:30:16 -0300

    validator

---
+++ vista/js/bootstrap/4.5.2/validator.js
@@ -0,0 +1,108 @@
+$("#formArmar").bootstrapValidator({
+  /* armarArchivo.php */
+  message: "Este valor no es valido",
+
+  feedbackIcons: {
+    valid: "fa fa-check",
+
+    invalid: "fas fa-times",
+
+    validating: "fa fa-circle",
+  },
+
+  fields: {
+    nombreArchivo: {
+      validators: {
+        notEmpty: {
+          message: "El nombre del archivo es requerido",
+        },
+      },
+    },
+    usuario: {
+      validators: {
+        notEmpty: {
+          message: "Seleccione un usuario",
+        },
+      },
+    },
+  },
+});
+/* compartir archivo */
+$("#formCompartir").bootstrapValidator({
+  message: "Este valor no es valido",
+
+  feedbackIcons: {
+    valid: "fa fa-check",
+
+    invalid: "fas fa-times",
+
+    validating: "fa fa-circle",
+  },
+
+  fields: {
+    usuario: {
+      validators: {
+        notEmpty: {
+          message: "Seleccione un usuario",
+        },
+      },
+    },
+    
+  },
+});
+/* formulario Eliminar Archivo */
+$("#formEliminar").bootstrapValidator({
+  message: "Este valor no es valido",
+
+  feedbackIcons: {
+    valid: "fa fa-check",
+
+    invalid: "fas fa-times",
+
+    validating: "fa fa-circle",
+  },
+
+  fields: {
+    usuario: {
+      validators: {
+        notEmpty: {
+          message: "Seleccione un usuario",
+        },
+      },
+    },
+    motivoElim: {
+      validators: {
+        notEmpty: {
+          message: "Escriba el motivo de eliminacion",
+        },
+      },
+    },
+  },
+});
+/* formulario contenido crear carpera*/
+$("#formCrearCarpeta").bootstrapValidator({
+  message: "Este valor no es valido",
+
+  feedbackIcons: {
+    valid: "fa fa-check",
+
+    invalid: "fas fa-times",
+
+    validating: "fa fa-circle",
+  },
+
+  fields: {
+    nombreCarpeta: {
+      validators: {
+        notEmpty: {
+          message: "Ingrese un Nombre de Carpeta",
+        },
+        /* regexp: {
+          regexp: /^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/,
+          message: " Ingrese una cadena válida",
+        }, */
+      },
+    },
+    
+  },
+});
